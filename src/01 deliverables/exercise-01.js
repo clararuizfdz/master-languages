efef54612d3cde432e17598df6c03c6cecc27379
Clara Ruiz Fernandez <clara.ruizfernandez@tui.be>
2020-05-29 20:36:07 +0200

    Implement array exercises

---
+++ src/01 deliverables/exercise-01.js
@@ -0,0 +1,40 @@
+console.log("************** DELIVERABLE 01 *********************");
+console.log("************** ARRAY OPERATIONS *******************");
+
+const listNumbers = [1,2,3,4,5];
+const listPersons = ["Ana","Maria","Antonio", "Pedro"];
+// ----------------------------Head------------------------------------------
+// Implementa una función head (inmutable), tal que, dado un array como entrada
+// extraiga y devuelva su primer elemento. Utiliza destructuring
+
+const head = ([first]) => first;
+console.log("head", head(listNumbers));
+console.log("head", head(listPersons));
+
+// ----------------------------Tail------------------------------------------
+// Implementa una función tail (inmutable), tal que, dado un array como entrada 
+// devuelta todos menos el primer elemento. Utiliza rest operator.
+
+const tail = ([,...elements]) => elements;
+console.log("Tail", tail(listPersons));
+console.log("Tail", tail(listNumbers));
+
+// ----------------------------Init------------------------------------------
+// Implementa una función init (inmutable), tal que, dado un array como 
+// entrada devuelva todos los elementos menos el último. 
+// Utiliza los métodos que ofrece Array.prototype.
+
+const init = (items) =>{   
+    return items.slice(0,-1);
+}
+console.log("Init", init(listNumbers));
+console.log("Init", init(listPersons));
+
+// ----------------------------Last------------------------------------------
+// Implementa una función last (inmutable), tal que, dado un array como 
+// entrada devuelva el último elemento.
+const last = (items) =>{   
+    return items[items.length-1];
+}
+console.log("Last", last(listNumbers));
+console.log("Last", last(listPersons));
