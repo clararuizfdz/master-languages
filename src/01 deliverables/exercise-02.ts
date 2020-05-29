b7ad01ead54ab61d6a13f70c8c53bf49343bdb8a
Clara Ruiz Fernandez <clara.ruizfernandez@tui.be>
2020-05-29 21:47:07 +0200

    Implement Concat functions

---
+++ src/01 deliverables/exercise-02.ts
@@ -0,0 +1,14 @@
+console.log("************** DELIVERABLE 02 *********************");
+console.log("****************** Concat *************************");
+
+// Implementa una función concat (inmutable) tal que, dados 2 arrays como 
+// entrada, devuelva la concatenación de ambos. Utiliza rest / spread operators.
+const concat = (a: number[],b: number[]) => [...a, ...b];
+
+console.log("Concat:", concat([1,2,3],[4,5,6]));
+
+// Implementa una versión del ejercicio anterior donde se acepten múltiples 
+// arrays de entrada (más de 2).
+
+const concatMultiple = (...args: number[][]) => args.reduce(concat);
+console.log("ConcatMultiple:", concatMultiple([1,2,3],[4,5,6],[7,8,9]));
